c25d806acf3f1258cb7ae95492bd98f0eeff544a
Brais <brais.alvarez@enti.cat>
2021-11-06 14:38:28 +0100

    Joystick and Camera

---
+++ Walk_Them_Up/Assets/CharacterMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    protected Joystick joystick;
+    protected Rigidbody rb;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(joystick.Horizontal * 10f, 0, joystick.Vertical * 10f);
+        
+    }
+}

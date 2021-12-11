cb392e2abf902ad240ee3436c69a62cf296e263a
Brais <brais.alvarez@enti.cat>
2021-12-11 12:56:18 +0100

    Enemies Basic IA

---
+++ Walk_Them_Up/Assets/Enemy_Generator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Generator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer = 0;
+    public GameObject [] enemy;
+   
+    void Start()
+    {
+        timer = Random.Range(5, 25);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <0)
+        {
+
+            Instantiate(enemy[Random.Range(0, enemy.Length)], new Vector3(this.transform.position.x + Random.Range(-50, 50), 1012, this.transform.position.z + Random.Range(-50, 50)), Quaternion.identity);
+            timer = Random.Range(5, 25);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
